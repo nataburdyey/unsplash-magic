93519a30a0c4032884087eee9bde6fb68bb51c2b
Natalia Burdyey <nata.burdey@gmail.com>
2023-07-03 13:03:47 -0400

    complete project

--- src/SearchForm.jsx
+++ src/SearchForm.jsx
@@ -1,6 +1,31 @@
+import { useGlobalContext } from "./context";
+
 const SearchForm = () => {
+  const { setSearchTerm } = useGlobalContext();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const searchValue = e.target.elements.search.value;
+    console.log(searchValue)
+    if (!searchValue) return;
+    setSearchTerm(searchValue);
+  };
+
   return (
-    <div>SearchForm</div>
-  )
-}
-export default SearchForm
+    <section>
+      <h1 className="title">Unsplash Photo Gallery</h1>
+      <form className="search-form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          className="form-input search-input"
+          name="search"
+          placeholder="office"
+        />
+        <button type="submit" className="btn">
+          Search
+        </button>
+      </form>
+    </section>
+  );
+};
+export default SearchForm;
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
 import App from "./App";
 import "./index.css";
 import { AppProvider } from "./context";
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <AppProvider>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools initialIsOpen={true} />
+    </QueryClientProvider>
   </AppProvider>
 );
